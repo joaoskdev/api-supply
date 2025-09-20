e5fb20519b07db6010950fdf2d1974a8fa820f14
Joao Sabakeviski <130368135+joaoskdev@users.noreply.github.com>
2025-09-20 12:04:43 +0000

    correcao de cors

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./rotas/auth");
 const privateRoutes = require("./rotas/private");
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors({ origin: 'https://supply-qzpq.onrender.com' }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api", privateRoutes);
